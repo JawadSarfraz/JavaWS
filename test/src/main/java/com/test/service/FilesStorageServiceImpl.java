044377ddc7b1cda21e6285c56f7b66c1e5a30aa9
JawadSarfraz <jawadsarfraz95@gmail.com>
2020-10-30 13:52:55 +0100

    Upload mutiple images

--- test/src/main/java/com/test/service/FilesStorageServiceImpl.java
+++ test/src/main/java/com/test/service/FilesStorageServiceImpl.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FilesStorageServiceImpl implements FilesStorageService {
 
-  private final Path root = Paths.get("/home/jawad/Desktop/uploads");
+  private final Path root = Paths.get("/home/uploads");
 
   @Override
   public void init() {
