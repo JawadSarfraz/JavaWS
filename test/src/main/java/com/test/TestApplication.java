ba6ac4d8663c1e0279e1d59ab26962e2bf87f248
JawadSarfraz <jawadsarfraz95@gmail.com>
2020-10-30 13:22:33 +0100

    Upload mutiple images

--- test/src/main/java/com/test/TestApplication.java
+++ test/src/main/java/com/test/TestApplication.java
@@ -2,13 +2,20 @@ package com.test;
 
 
 import org.modelmapper.ModelMapper;
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.test.service.FilesStorageService;
+
 @SpringBootApplication
-public class TestApplication {
+public class TestApplication implements CommandLineRunner{
 	
+	@Resource
+	  FilesStorageService storageService;
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
@@ -16,5 +23,13 @@ public class TestApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 	    return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		storageService.deleteAll();
+	    storageService.init();
+		
 	}
 }
